714179fa327340c08869242afd486f6d7d5343d1
jwenjian <1639301503@qq.com>
2019-10-03 15:10:36 +0800

    do not run in github action

--- usda_fruit.py
+++ usda_fruit.py
@@ -3,18 +3,22 @@ from bs4 import BeautifulSoup
 
 IMG_FOLDER = 'fruit_images/'
 
+
 def run():
     for (idx, page) in enumerate(range(380)):
         resp = requests.get(
             'https://usdawatercolors.nal.usda.gov/pom/search.xhtml?start={}&searchText=&searchField=&sortField='.format(
-                page * 20))
+                idx * 20))
         soup = BeautifulSoup(resp.text, 'html.parser')
         for (div_idx, div) in enumerate(soup.select('div.document')):
             doc = div.select_one('dl.defList')
             artist = doc.select_one(':nth-child(2)>a').get_text()
             year = doc.select_one(':nth-child(4)>a').get_text()
-            scientific_name = doc.select_one(':nth-child(6)>a').get_text()
-            common_name = doc.select_one(':nth-child(8)>a').get_text()
+            # cannot parse scientific name or common name for some pictures, just use 'none' instead to avoid terminating
+            scientific_name = 'none' if doc.select_one(':nth-child(6)>a') is None else doc.select_one(
+                ':nth-child(6)>a').get_text()
+            common_name = 'none' if doc.select_one(':nth-child(8)>a') is None else doc.select_one(
+                ':nth-child(8)>a').get_text()
             thumb_pic_src = div.select_one('div.thumb-frame>a>img')['src']
             id = (idx + 1) * 20 + div_idx + 1
             info = FruitInfo(id, artist, year, scientific_name, common_name, thumb_pic_src)
@@ -52,10 +56,6 @@ class FruitInfo:
                                                                                                         self.scientific_name,
                                                                                                         self.common_name,
                                                                                                         self.thumb_pic_url)
-
-
-#
-# soup = BeautifulSoup(html_doc, 'html.parser')
 
 
 if __name__ == '__main__':
